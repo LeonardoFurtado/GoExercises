e3f43678041d2fa4929661a88eaa113fd87d393c
Leonardo Furtado <srleonardofurtado@gmail.com>
2022-03-04 20:40:55 -0300

    feat: hello world tests final.

--- GoWithTests/hello_world/hello.go
+++ GoWithTests/hello_world/hello.go
@@ -2,10 +2,37 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanish = "Spanish"
+const french = "French"
+const portuguese = "Portuguese"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const portugueseHelloPrefix = "Olá, "
+
+func gettingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case portuguese:
+		prefix = portugueseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return gettingPrefix(language) + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "english"))
 }
